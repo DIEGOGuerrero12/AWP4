f106686fe533c76fdd7a708a99560b55422027a2
DIEGOGuerrero12 <160614103+DIEGOGuerrero12@users.noreply.github.com>
2025-01-30 18:49:44 +0000

    Mensaje descriptivo de los cambios

--- 10_webapp/app.py
+++ 10_webapp/app.py
@@ -2,12 +2,34 @@ import web
 
 urls = (
     '/', 'Index',
+    '/login', 'Login'
 )
+
 app = web.application(urls, globals())
 render = web.template.render('views/')
+
 class Index:
     def GET(self):
+        print("Página principal cargada")  # <-- Agrega esto
         return render.index()
 
+class Login:
+    def GET(self):
+        print("Página de login cargada")  # <-- Agrega esto
+        return render.login()
+
+    def POST(self):
+        user_data = web.input()
+        username = user_data.username
+        password = user_data.password
+        
+        print(f"Intento de login: {username} - {password}")  # <-- Agrega esto
+        
+        if username == "admin" and password == "1234":
+            return "Bienvenido, " + username
+        else:
+            return "Credenciales incorrectas. <a href='/login'>Intenta de nuevo</a>"
+
 if __name__ == "__main__":
+    print("Iniciando servidor en http://localhost:8080/")  # <-- Agrega esto
     app.run()
